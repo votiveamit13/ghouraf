3540df63aa666a19534521633afb65e85ed470c8
votiveamit13 <votiveamit.web@gmail.com>
2025-08-27 12:18:19 +0530

    fix domain issue

--- frontend/src/components/public/Navbar.jsx
+++ frontend/src/components/public/Navbar.jsx
@@ -68,7 +68,7 @@ const handleRegister = async (e) => {
   }
 setRegisterLoading(true);
   try {
-    await axios.post(`http://216.10.243.87:3000/api/auth/register`, {
+    await axios.post(`http://127.0.0.1:3000/api/auth/register`, {
       firstName: form.firstName,
       lastName: form.lastName,
       email: form.email,
