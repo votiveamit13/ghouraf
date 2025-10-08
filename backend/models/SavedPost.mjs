3d21eb6e50a04fc00681cf4dc79bad6d039ef56b
Amit Patel <votiveamit.web@gmail.com>
2025-10-08 15:27:57 +0530

    saved post API

--- backend/models/SavedPost.mjs
+++ backend/models/SavedPost.mjs
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const savedPostSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    postCategory: { type: String, enum: ["Space", "TeamUp", "SpaceWanted"], required: true },
+    postCategory: { type: String, enum: ["Space", "Teamup", "SpaceWanted"], required: true },
     postId: { type: mongoose.Schema.Types.ObjectId, required: true },
     snapshot: { type: Object },
     createdAt: { type: Date, default: Date.now }
