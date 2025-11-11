8a6f23d31a524ba3a454dcc207dd1c499c5c6e7f
votiveamit13 <votiveamit.web@gmail.com>
2025-11-11 15:33:31 +0530

    notification module

--- backend/config/firebase.mjs
+++ backend/config/firebase.mjs
@@ -2,7 +2,6 @@
 import admin from "firebase-admin";
 import fs from "fs";
 
-if (!admin.apps.length) {
 const serviceAccount = JSON.parse(
   fs.readFileSync(new URL("../serviceAccountKey.json", import.meta.url))
 );
@@ -10,9 +9,5 @@ const serviceAccount = JSON.parse(
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-}
 
 export const authAdmin = admin.auth();
-export const dbAdmin = admin.firestore();
-export const messagingAdmin = admin.messaging();
-export default admin;
